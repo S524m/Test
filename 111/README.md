088bc593ddc38190bb4a8c6682818feee1a6dec4
S524m <130961070+S524m@users.noreply.github.com>
2023-06-05 13:58:33 +0800

    Add files via upload

---
+++ 111/README.md
@@ -0,0 +1,4 @@
+# 教學樣式頁面
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/moonacloud/pen/KKxQoJz](https://codepen.io/moonacloud/pen/KKxQoJz).
+
---
+++ 111/dist/script.js
@@ -0,0 +1,9 @@
+document.write("  ")
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
